14388256b0ca6a8651abfd892c04331e824b8cdb
Carlos Adriano <cadrianogamac@gmail.com>
2024-03-06 18:55:14 -0300

    feat: create tagRepository CRUD functions

--- src/tag/repository/repository.go
+++ src/tag/repository/repository.go
@@ -40,27 +40,30 @@ func (repository *TagRepository) FindAll() ([]tagModel.Tag, error) {
 	return tags, nil
 }
 
-func (repository *TagRepository) FindById(id string) (tagModel.Tag, error) {
+func (repository *TagRepository) FindByID(id string) (tagModel.Tag, error) {
 	var tag tagModel.Tag
 	err := repository.db.QueryRow(context.Background(),
 		`
     SELECT * FROM tag
     WHERE id = $1 
     `, id).Scan(&tag)
+
 	if err != nil {
 		return tag, err
 	}
+
 	return tag, nil
 }
 
 func (repository *TagRepository) Create(tag tagModel.Tag) error {
 	args := pgx.NamedArgs{
-		"id":         tag.Id,
+		"id":         tag.ID,
 		"nome":       tag.Nome,
 		"cor":        tag.Cor,
 		"created_at": tag.CreatedAt,
 		"updated_at": tag.UpdatedAt,
 	}
+
 	_, err := repository.db.Exec(context.Background(),
 		`
     INSERT INTO tag
@@ -68,5 +71,35 @@ func (repository *TagRepository) Create(tag tagModel.Tag) error {
     VALUES 
     (@id, @nome, @cor, @updated_at, @created_at)
     `, args)
+
+	return err
+}
+
+func (repository *TagRepository) Update(tag tagModel.Tag) error {
+	args := pgx.NamedArgs{
+		"id":         tag.ID,
+		"nome":       tag.Nome,
+		"cor":        tag.Cor,
+		"created_at": tag.CreatedAt,
+		"updated_at": tag.UpdatedAt,
+	}
+
+	_, err := repository.db.Exec(context.Background(),
+		`
+    UPDATE tag
+    SET nome = @nome, cor = @cor, updated_at = @updated_at
+    WHERE id = @id
+    `, args)
+
+	return err
+}
+
+func (repository *TagRepository) Delete(id string) error {
+	_, err := repository.db.Exec(context.Background(),
+		`
+    DELETE FROM tag
+    WHERE id = $1
+    `, id)
+
 	return err
 }
